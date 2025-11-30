efe580360cf92fb98e4d1a440eff18d0354a1901
Oğuzhan Berke <ozdil@student.agh.edu.pl>
2025-11-30 16:20:40 +0300

    initial

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,2 @@
-"""This is where I initialize the main package for our log analyzer.
-
-I'm keeping track of the version and author info here so we can reference it
-throughout the application.
-"""
-
 __version__ = "0.1.0"
 __author__ = "Smart Log Analyzer Team"
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,1 @@
-"""Here's where I'll organize all the tests for our log analyzer.
 
-As we build out features, I'll add corresponding test files here to make
-sure everything works as expected.
-"""
